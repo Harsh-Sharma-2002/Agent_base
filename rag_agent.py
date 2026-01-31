fe25f502d8cae63715382316c5b5b37c631256a6
Harsh Sharma <hs210310222360@gmail.com>
2026-01-30 22:25:15 -0700

    file path corrections

--- rag_agent.py
+++ rag_agent.py
@@ -48,7 +48,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 pages_split = text_splitter.split_documents(pages) # We now apply this to our pages
 
-persist_directory = r"C:\Vaibhav\LangGraph_Book\LangGraphCourse\Agents"
+persist_directory = r"/Users/harshsharma/Desktop/Work_Dir/lang_graph"
 collection_name = "stock_market"
 
 # If our collection does not exist in the directory, we create using the os command
